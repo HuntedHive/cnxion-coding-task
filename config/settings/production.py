f8d83746b00817f0b1a0e63c60f4724760683d21
Nathan Keilar <325430+nkeilar@users.noreply.github.com>
2025-05-17 19:47:28 +1000

    Fix typo in production logging comment

--- config/settings/production.py
+++ config/settings/production.py
@@ -156,7 +156,7 @@ AWS_PRELOAD_METADATA = True
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#logging
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
-# the site admins bon every HTTP 500 error when DEBUG=False.
+# the site admins on every HTTP 500 error when DEBUG=False.
 # See https://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
